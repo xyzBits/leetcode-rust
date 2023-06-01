dcb92287ab6fd69affad52105c9ab07893a02ad8
dongfang <lidf077@gmail.com>
2023-06-01 10:53:52 +0800

    2sum multiple approach

--- src/solution/s0001_two_sum.rs
+++ src/solution/s0001_two_sum.rs
@@ -1,14 +1,13 @@
 use std::collections::HashMap;
 
-pub struct Solution {}
-
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        if nums.len() == 0 {
+        if nums.is_empty() {
             return vec![];
         }
 
+        // let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
         let mut map = HashMap::with_capacity(nums.len());
 
         for (index, value) in nums.iter().enumerate() {
@@ -16,18 +15,69 @@ impl Solution {
 
             match map.get(&remain) {
                 Some(remain_index) => {
-                    return vec![*remain_index, index as i32];
-                }
+                    return vec![*remain_index as i32, index as i32];
+                },
                 None => {
-                    map.insert(*value, index as i32);
+                    map.insert(*value, index);
+                },
+            }
+        }
+
+
+        vec![]
+    }
+
+
+    pub fn two_sum_brute_force(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        if nums.is_empty() {
+            return vec![];
+        }
+
+
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                // if *nums.get(i).unwrap() + *nums.get(j).unwrap() == target {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
                 }
             }
         }
 
-        return vec![];
+        vec![]
+    }
+
+    pub fn two_sum_with_2pass_hash_table(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        if nums.is_empty() {
+            return vec![];
+        }
+
+        let mut map = HashMap::with_capacity(nums.len());
+
+        for i in 0..nums.len() {
+            map.insert(nums[i], i);
+        }
+
+
+        for i in 0..nums.len() {
+            let complement  = target - nums[i];
+
+            match map.get(&complement) {
+                Some(complement_index) => {
+                    if i != *complement_index {
+                        return vec![*complement_index as i32, i as i32];
+                    }
+                },
+                _ => {}
+            }
+        }
+
+
+        vec![]
     }
 }
 
+
+pub struct Solution {}
 
 #[cfg(test)]
 mod tests {
